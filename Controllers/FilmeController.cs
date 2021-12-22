ec310cf78dc613ba8346ae56f8ad7090c9386f34
Arthur Ferreira <arthur.ferreira@compasso.com.br>
2021-12-22 13:53:32 -0300

    Primeiro commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,48 @@
+﻿using FilmesAPIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPIs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(ListaDeUmFilme), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult ListaDeFilmes()
+        {
+            return Ok(filmes);
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult ListaDeUmFilme(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(Filme => Filme.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+        
+        
+        
+    }
+
+}
+
